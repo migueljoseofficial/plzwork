7cd2ba898df6e70ac36114969e15e6c6b3fdf147
migueljoseoffical <migueljoseofficial>
2024-03-23 13:02:06 -0400

    started some css and the navbar

---
+++ website/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">Portfolio</a>
+      <div className={styles.menu}>
+        <ul className={styles.menuitems}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
+
+      </div>
+
+
+    </nav>
+
+
+  );
+};
